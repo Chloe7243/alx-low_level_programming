ba22c2ddf4fa6f0dd8a5682a64b20bfce8773bbd
Stephanie <Stephaniechloe21@gmail.com>
2023-03-10 15:47:11 -0800

    Tasks

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
-#include "holberton.h"
 
 /**
  * main - add args
@@ -19,7 +17,7 @@ int main(int argc, char *argv[])
 
 	while (i < argc)
 	{
-		if (isdigit(*argv[i]) == 0)
+		if (atoi(argv[i]) == 0)
 		{
 			printf("Error\n");
 			return (1);

608f71e17676b65a651a7cd6675b28769551ba29
Stephanie <Stephaniechloe21@gmail.com>
2023-03-10 13:06:58 -0800

    Task 2

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints number of args
+ *
+ * @argc: argument count
+ * @argv: string of arguments
+ *
+ * Return: integer
+*/
+
+int main(int argc, char __attribute__((__unused__)) *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}

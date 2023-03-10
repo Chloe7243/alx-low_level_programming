2926e4aea0f97d27b2b68a71361860c37cb96d9a
Stephanie <Stephaniechloe21@gmail.com>
2023-03-10 14:13:13 -0800

    Task 5

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - add args
+ *
+ * @argc: argument count
+ * @argv: string of arguments
+ *
+ * Return: integer
+*/
+
+int main(int argc, char *argv[])
+{
+	int i = 1;
+	int sum = 0;
+
+	while (i < argc)
+	{
+		if (atoi(argv[i]) == 0)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		sum += atoi(argv[i]);
+		i++;
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}

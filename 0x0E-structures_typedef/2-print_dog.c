aa7760c9e842cdb62e451f3fe675e454046340ee
Stephanie <Stephaniechloe21@gmail.com>
2023-03-20 12:32:32 -0700

    Fixes

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,7 +14,7 @@ void print_dog(struct dog *d)
 	if (d != NULL)
 	{
 		printf("Name: %s\n", d->name ? d->name : "nil");
-		printf("Age: %f\n", d->age);
+		printf("Age: %s\n", "nil");
 		printf("Owner: %s\n", d->owner ? d->owner : "nil");
 	}
 }

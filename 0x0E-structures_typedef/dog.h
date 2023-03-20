f2470f637cc0ed0e84ea1ed9cccb406a5f8967a2
Stephanie <Stephaniechloe21@gmail.com>
2023-03-20 09:55:13 -0700

    Task 1

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
-struct dog {
+/**
+ * struct dog - Struct for dog
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ */
+
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;

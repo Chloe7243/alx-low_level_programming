1ba07b9c1504cdafc938fe4928ea9bd40d340a53
Stephanie <Stephaniechloe21@gmail.com>
2023-03-22 14:58:09 -0700

    Task 3

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * int_index - returns the index of the first element for which the cmp matches
+ *
+ * @array: name of the array
+ * @size: size of array
+ * @cmp: comparison function
+ *
+ * Return: integer.
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == NULL || cmp == NULL)
+		return (0);
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+		{
+			return (i);
+		}
+	}
+	return (0);
+}

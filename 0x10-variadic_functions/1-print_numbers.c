b0bf4ec050809d7fcb7496cd2ecb9dff188b285c
Stephanie <Stephaniechloe21@gmail.com>
2023-03-23 06:19:44 -0700

    TASK 2

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -20,7 +20,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(ap, int));
-		printf("%c", (i != (n - 1)) ? *separator : '\n');
+		(separator && i != n - 1) ? printf("%c ", *separator) : putchar('\n');
 	}
 	va_end(ap);
 }

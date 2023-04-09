7f3ac5fa9dfa6208c3613c14b619ecdd4f1352a8
Stephanie <Stephaniechloe21@gmail.com>
2023-04-09 14:33:41 -0700

    task 1 fix

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -11,23 +11,14 @@
 void print_binary(unsigned long int n)
 {
 	unsigned long int max = UINT_MAX << 31;
-	int is_zero = 1;
 
-	while (max > 0 && n > 0)
+	while (max > 0)
 	{
 		if ((n & max) != 0)
-		{
-			is_zero = 0;
 			_putchar('1');
-		}
 		else
-		{
-			if (is_zero == 0)
-				_putchar('0');
-		}
+			_putchar('0');
 		max >>= 1;
 	}
 
-	if (is_zero)
-		_putchar('0');
 }

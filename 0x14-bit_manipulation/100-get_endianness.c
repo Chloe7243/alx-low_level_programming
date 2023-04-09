f64297c6e6605f3e451b4606b775432290414b7a
Stephanie <Stephaniechloe21@gmail.com>
2023-04-09 12:30:49 -0700

    Fix Endianness

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 
+/**
+ * get_endianness - get endianness of system;
+ * Return: int;
+ */
+
 int get_endianness(void)
 {
 	int num = 0x12345678;
 	char *ptr = (char *)&num;
-	
+
 	if (ptr[0] == 78)
-		return (1);
-	return 0;
+		return (0);
+	return (1);
 }

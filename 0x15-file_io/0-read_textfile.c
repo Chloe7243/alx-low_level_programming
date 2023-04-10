86249f9e9f3482621ff4d8bbe223020cdadaa031
Stephanie <Stephaniechloe21@gmail.com>
2023-04-10 12:49:52 -0700

    Task 0 fix

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -33,30 +33,30 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	r_val = fread(buf, sizeof(char), letters, fp);
 	if (r_val < 0)
-	{
-		fclose(fp);
-		free(buf);
-		return (0);
-	}
+		return (help_fnc(buf, fp));
 
 	w_val = fwrite(buf, sizeof(char), r_val, stdout);
 	if (w_val != r_val)
-	{
-		fclose(fp);
-		free(buf);
-		return (0);
-	}
-
-	if (w_val != r_val)
-	{
-		fclose(fp);
-		free(buf);
-		return (0);
-	}
-
+		return (help_fnc(buf, fp));
+		
 	fclose(fp);
 	free(buf);
 
 	return (w_val);
 
+}
+
+/**
+ * help_fnc - ...
+ *
+ * @buffer: ...
+ * @file_ptr: ...
+ * Return: int
+ */
+
+int help_fnc(char *buffer, FILE *file_ptr)
+{
+	fclose(file_ptr);
+	free(buf);
+	return (0);
 }

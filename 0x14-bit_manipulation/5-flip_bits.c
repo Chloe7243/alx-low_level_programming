fe54e63dc830766eadea6dddf5427ecd3e97a9b9
Stephanie <Stephaniechloe21@gmail.com>
2023-04-10 08:15:55 -0700

    fix

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -12,9 +12,9 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	unsigned long int b = n ^ m, value = 1;
 	long int index = 0, count = 0;
 
-	while ((value << index) <= b && b)
+	while ((value << index) <= b)
 	{
-		if ((b & (value << index)) != 0)
+		if ((b & (value << index)))
 			count++;
 		index++;
 	}

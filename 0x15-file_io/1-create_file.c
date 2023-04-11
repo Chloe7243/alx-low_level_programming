1ad26c75ee476fa46dc0498cba8b4525410be5b2
Stephanie <Stephaniechloe21@gmail.com>
2023-04-11 07:19:01 -0700

    task 1 fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,11 +19,11 @@ int create_file(const char *filename, char *text_content)
 	if (fildes < 0)
 		return (-1);
 
-	while (text_content[size])
-		size++;
 
 	if (text_content)
 	{
+		while (text_content[size])
+			size++;
 		w_val = write(fildes, text_content, size);
 		if (w_val != size)
 			return (-1);

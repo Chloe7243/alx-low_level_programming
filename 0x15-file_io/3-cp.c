02fdb09d914ff3ddd8ad769541de95d327b640e2
Stephanie <Stephaniechloe21@gmail.com>
2023-04-11 09:01:58 -0700

    task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -15,32 +15,25 @@ int main(int ac, char **av)
 	int fd, fd2, fc, fc2, w_val, r_val;
 
 	if (ac != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
+		handle_error("Usage: cp file_from file_to\n", 97, "");
 
 	f_from = av[1];
 	f_to = av[2];
 	fd = open(f_from, O_RDONLY);
 	fd2 = creat(f_to, 0664);
 
+	if (fd < 0)
+		handle_error("Error: Can't read from file %s\n", 98, f_from);
+
 	while ((r_val = read(fd, buf, 1024)) > 0)
 	{
 		w_val = write(fd2, buf, r_val);
 		if (fd2 < 0 || w_val != r_val)
-		{
-			dprintf(STDERR_FILENO, "Can't write to %s\n", f_to);
-			exit(99);
-		}
+			handle_error("Can't write to %s\n", 99, f_to);
 	}
 
-	if (fd < 0 || r_val < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", f_from);
-		exit(98);
-
-	}
+	if (r_val < 0)
+		handle_error("Error: Can't read from file %s\n", 98, f_from);
 
 	fc = close(fd);
 	fc2 = close(fd2);
@@ -52,4 +45,24 @@ int main(int ac, char **av)
 	}
 
 	return (0);
+}
+
+/**
+ * handle_error - handle errors
+ *
+ * @err_msg: ...
+ * @status: ...
+ * @fnc: ...
+ * Return: e
+ */
+
+void handle_error(char *err_msg, int status, const char *fnc)
+{
+	char *str;
+
+	str = malloc(1024);
+	sprintf(str, err_msg, fnc);
+	dprintf(STDERR_FILENO, "%s", str);
+	free(str);
+	exit(status);
 }

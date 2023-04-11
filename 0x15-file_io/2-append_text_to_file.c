5593ee636e92d210dc5e97ee7c4f72ca5d2ece1b
Stephanie <Stephaniechloe21@gmail.com>
2023-04-11 07:20:40 -0700

    task 2 fix

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -19,9 +19,6 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (fildes < 0)
 		return (-1);
 
-	while (text_content[size])
-		size++;
-
 	if (text_content)
 	{
 		w_val = write(fildes, text_content, size);

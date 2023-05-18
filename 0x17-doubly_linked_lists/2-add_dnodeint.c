800980e1f2c6f1ffe3d844a2f83146670b37b80d
Stephanie <Stephaniechloe21@gmail.com>
2023-05-18 05:18:35 -0700

    task 2

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -16,9 +16,9 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		return (NULL);
 
 	new->n = n;
-	new->next = *head;
+	new->next = (*head);
 	new->prev = NULL;
-	*head = new;
+	(*head) = new;
 
 	return (*head);
 }

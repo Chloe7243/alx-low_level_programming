b45e7fef93228a3ee519decbcdbad827d2d80869
Stephanie <Stephaniechloe21@gmail.com>
2023-05-18 08:17:13 -0700

    task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -18,7 +18,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 
 	if (idx == 0)
-		return (add_dnodeint(&*h, n));
+		return (add_dnodeint(h, n));
 
 	temp = *h;
 	new->n = n;
@@ -38,7 +38,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	}
 
 	if (idx == i)
-		return (add_dnodeint_end(&*h, n));
+		return (add_dnodeint_end(h, n));
 
 	return (NULL);
 }

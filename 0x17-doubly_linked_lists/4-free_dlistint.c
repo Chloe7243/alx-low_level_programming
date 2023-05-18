406a3abfb5863c562c7fa3a0d610ba1411f45769
Stephanie <Stephaniechloe21@gmail.com>
2023-05-18 06:14:48 -0700

    task 4

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -9,11 +9,11 @@
 void free_dlistint(dlistint_t *head)
 {
 	dlistint_t *temp;
-	temp = head;
 
-	while (temp)
+	temp = head;
+	while(temp->next)
 	{
 		free(temp);
-		temp = temp->next;
+		temp = temp->next
 	}
 }

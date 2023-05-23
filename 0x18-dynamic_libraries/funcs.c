a1714ecf79c021fcd8b45fa96061d2716b7269d9
Stephanie <Stephaniechloe21@gmail.com>
2023-05-22 21:30:54 -0700

    task 0 fix

--- 0x18-dynamic_libraries/funcs.c
+++ 0x18-dynamic_libraries/funcs.c
@@ -1,22 +1,122 @@
 #include "main.h"
+#include <stddef.h>
 
-int _putchar(char c){};
-int _islower(int c){};
-int _isalpha(int c){};
-int _abs(int n){};
-int _isupper(int c){};
-int _isdigit(int c){};
-int _strlen(char *s){};
-void _puts(char *s){};
-char *_strcpy(char *dest, char *src){};
-int _atoi(char *s){};
-char *_strcat(char *dest, char *src);
-char *_strncat(char *dest, char *src, int n);
-char *_strncpy(char *dest, char *src, int n);
-int _strcmp(char *s1, char *s2){};
-char *_memset(char *s, char b, unsigned int n){};
-char *_memcpy(char *dest, char *src, unsigned int n){};
-char *_strchr(char *s, char c){};
-unsigned int _strspn(char *s, char *accept){};
-char *_strpbrk(char *s, char *accept){};
-char *_strstr(char *haystack, char *needle){};
+int _putchar(char c)
+{
+	/* Your code here */
+	return 0;
+}
+
+int _islower(int c)
+{
+	/* Your code here */
+	return 0;
+}
+
+int _isalpha(int c)
+{
+	/* Your code here */
+	return 0;
+}
+
+int _abs(int n)
+{
+	/* Your code here */
+	return 0;
+}
+
+int _isupper(int c)
+{
+	/* Your code here */
+	return 0;
+}
+
+int _isdigit(int c)
+{
+	/* Your code here */
+	return 0;
+}
+
+int _strlen(char *s)
+{
+	/* Your code here */
+	return 0;
+}
+
+void _puts(char *s)
+{
+	/* Your code here */
+}
+
+char *_strcpy(char *dest, char *src)
+{
+	/* Your code here */
+	return dest;
+}
+
+int _atoi(char *s)
+{
+	/* Your code here */
+	return 0;
+}
+
+char *_strcat(char *dest, char *src)
+{
+	/* Your code here */
+	return dest;
+}
+
+char *_strncat(char *dest, char *src, int n)
+{
+	/* Your code here */
+	return dest;
+}
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	/* Your code here */
+	return dest;
+}
+
+int _strcmp(char *s1, char *s2)
+{
+	/* Your code here */
+	return 0;
+}
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	/* Your code here */
+	return s;
+}
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	/* Your code here */
+	return dest;
+}
+
+char *_strchr(char *s, char c)
+{
+	/* Your code here */
+	return NULL;
+}
+
+unsigned int _strspn(char *s, char *accept)
+{
+	/* Your code here */
+	return 0;
+}
+
+char *_strpbrk(char *s, char *accept)
+{
+	/* Your code here */
+	return NULL;
+}
+
+char *_strstr(char *haystack, char *needle)
+{
+	/* Your code here */
+	return NULL;
+}
+

9fee1bd7e095e3971fcad1b16e3edc4f881da2c0
Stephanie <Stephaniechloe21@gmail.com>
2023-06-10 09:21:05 -0700

    task 0 fix

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,10 +9,11 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_table = malloc(sizeof(hash_table_t));
+
 	if (new_table == NULL)
 		return (NULL);
 	new_table->size = size;
-	new_table->array = calloc(new_table->size, sizeof(hash_node_t*));
+	new_table->array = calloc(new_table->size, sizeof(hash_node_t));
 	if (new_table->array == NULL)
 		return (NULL);
 
